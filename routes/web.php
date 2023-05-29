ada8c95d03d3c2daa8878a1993e6594d7e8936fc
syahnisahla <syahnisahlaaa727@gmail.com>
2023-05-29 10:45:53 +0700

    yes

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 use App\Http\Controllers\MonevController;
 use App\Http\Controllers\PtbController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WidgetController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('welcome'));
 
 Route::middleware(['auth', 'verified'])->group(function (): void {
     Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
+
 
     Route::resource('ptbs', PtbController::class);
     Route::resource('monevs', MonevController::class);
@@ -20,6 +22,8 @@ Route::middleware('auth')->group(function (): void {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', [WidgetController::class, 'showWidget'])->name('dashboard');
 });
+
 
 require __DIR__ . '/auth.php';
